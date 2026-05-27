4124030da9d6d26eb7db0debf9d3104809fd790b
giuseppebiemmecar-max <giuseppe.biemmecar@gmail.com>
2026-05-27 20:42:05 +0200

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer os_v2_app_oc5oifr4cffxvnn2slgj2r5ldyqyhuaph4gekrfaijj7jzwbnhnlxf36u7seiq6j7arituipsxiyxorh67rysrhoea4pg62aaay4uxi'
+        'Authorization': 'Bearer os_v2_app_oc5oifr4cffxvnn2slgj2r5ldymawzdtxgouhtv5zf5jw7myfl4mbq5i6erj52zo2gmmwrmogvrnwfds45mngoo2ecefmbydfnbrvma'
       },
       body: JSON.stringify({
         app_id: '70bae416-3c11-4b7a-b5ba-92cc9d47ab1e',
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
       })
     });
     const data = await r.json();
+    console.log('OneSignal:', JSON.stringify(data));
     if (!r.ok) return res.status(400).json({ error: data });
     return res.status(200).json({ success: true, recipients: data.recipients });
   } catch(err) {
